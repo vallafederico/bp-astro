9c3ad34b5dad718abc3ca09b9fc5579f68906442
vallafederico <valla.federico@gmail.com>
2022-11-26 17:15:55 +0100

    various updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
-
 import tailwind from "@astrojs/tailwind";
+// import glsl from "vite-plugin-glsl";
+
+export default defineConfig({
+  integrations: [preact(), tailwind()],
+  vite: {
+    // plugins: [glsl()],
+    assetsInclude: ["**/*.webp", "**/*.glb"],
+  },
+});
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [preact(), tailwind()]
-});
